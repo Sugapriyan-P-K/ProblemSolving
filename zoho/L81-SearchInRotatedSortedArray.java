e962e0661317952f8bfde40af2ef2c893c7e94ad
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2024-04-23 22:41:49 +0530

    Create L81-SearchInRotatedSortedArray.java

---
+++ zoho/L81-SearchInRotatedSortedArray.java
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[mid] == nums[left] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+                continue;
+            }
+            if (nums[left] <= nums[mid]) { // left sorted portion
+                if (nums[mid] >= target && nums[left] <= target) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && nums[right] >= target) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
