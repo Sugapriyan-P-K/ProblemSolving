cef3c9e9403c13d0fd6dbf3e75ed6218453b98d1
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2024-05-12 19:13:39 +0530

    deleted

--- Top_Interview_150_List_With_solution(leetcode)/Array|String/L55-jump_game.py
+++
@@ -1,9 +0,0 @@
-# https://leetcode.com/problems/jump-game/
-class Solution:
-    def canJump(self, nums: List[int]) -> bool:
-        numLen = len(nums)
-        goal = numLen - 1
-        for idx in range(numLen - 1, -1, -1):
-            if idx + nums[idx] >= goal:
-                goal = idx
-        return True if goal == 0 else False
