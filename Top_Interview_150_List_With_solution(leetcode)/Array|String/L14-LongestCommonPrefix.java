5459b8b83a721876680a39b1be8761920a7dc441
SUGAPRIYAN P K <sugapriyan2003@gmail.com>
2024-05-20 12:37:11 +0530

    L14-LongestCommonPrefix.java

---
+++ Top_Interview_150_List_With_solution(leetcode)/Array|String/L14-LongestCommonPrefix.java
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/longest-common-prefix/
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        if (strs.length == 0) {
+            return "";
+        }
+        String temp = strs[0];
+        for (int i = 0; i < strs.length; i++) {
+            while (strs[i].indexOf(temp) != 0) {
+                temp = temp.substring(0, temp.length() - 1);
+                if (temp.isEmpty()) {
+                    return "";
+                }
+            }
+        }
+        return temp;
+    }
+}
